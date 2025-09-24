87a67de981a30d98935bfd312b14f74283918f9e
IoBohoBoI <ioboh.martino@gmail.com>
2025-09-24 22:09:34 +0200

    Modifiche per testare la CI

--- test/integration_test/app_start_test.dart
+++ test/integration_test/app_start_test.dart
@@ -1,17 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:concessionario_supercar/main.dart' as app;
 
 void main() {
   IntegrationTestWidgetsFlutterBinding.ensureInitialized();
 
   testWidgets('Splash screen shows brand name', (tester) async {
-    app.main();
-    // primo frame
-    await tester.pumpAndSettle(const Duration(milliseconds: 100));
+    // Inizializza valori mock per SharedPreferences
+    SharedPreferences.setMockInitialValues({});
 
-    // verifica testo mostrato nella splash
+    // Avvia l'app
+    app.main();
+
+    // Attendi che il primo frame e tutti gli animazioni siano completate
+    await tester.pumpAndSettle(const Duration(seconds: 2));
+
+    // Verifica che il testo "CloperCar" sia presente
     expect(find.text('CloperCar'), findsOneWidget);
   });
 }
